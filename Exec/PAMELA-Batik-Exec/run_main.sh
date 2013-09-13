9ffc231b88d235efc008ade68650bec8b7db1e07
pconesa <p.conesa.mingo@gmail.com>
2013-09-13 17:14:07 +0100

    scripts for running pipelines from the command line using maven exec.

---
+++ Exec/PAMELA-Batik-Exec/run_main.sh
@@ -0,0 +1,1 @@
+mvn -e -X exec:java -Dexec.mainClass="uk.ac.ebi.pamela.layoutpipeline.exec.PAMELAPipelineExec"
---
+++ Exec/PAMELA-Batik-Exec/run_main_list.sh
@@ -0,0 +1,1 @@
+mvn -e -X exec:java -Dexec.mainClass="uk.ac.ebi.pamela.layoutpipeline.exec.PAMELAMultiQueryPipelineExec" -Dexec.args="./ ../Rhea-Batik-Exec/src/test/resources/chebiids.txt"
